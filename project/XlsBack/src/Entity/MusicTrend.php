2f745ad4d1ef24c354edf05bbad815a3ba9c6bb1
alex <alexandre.buathier@free.fr>
2022-06-30 11:03:41 +0200

    Entity MusicTrend: setup collections & items operations

--- project/XlsBack/src/Entity/MusicTrend.php
+++ project/XlsBack/src/Entity/MusicTrend.php
@@ -9,7 +9,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MusicTrendRepository::class)]
-#[ApiResource]
+#[ApiResource(collectionOperations: ["GET"], itemOperations: ["GET"])]
 class MusicTrend
 {
     #[ORM\Id]
