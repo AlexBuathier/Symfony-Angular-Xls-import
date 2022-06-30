02ed9361452310b806004d13f6e6b0d3d5344a97
alex <alexandre.buathier@free.fr>
2022-06-30 10:27:56 +0200

    Fixtures:  Install & setup dependencies

--- project/XlsBack/config/bundles.php
+++ project/XlsBack/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
